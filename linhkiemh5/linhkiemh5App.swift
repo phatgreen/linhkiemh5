1f6d360f9881f1f443373a55a01f3e277f601be3
Phát Green <phatgreen.dev>
2023-10-27 15:03:59 -0700

    Initial Commit

---
+++ linhkiemh5/linhkiemh5App.swift
@@ -0,0 +1,17 @@
+//
+//  linhkiemh5App.swift
+//  linhkiemh5
+//
+//  Created by Vũ Tấn Phát on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct linhkiemh5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
